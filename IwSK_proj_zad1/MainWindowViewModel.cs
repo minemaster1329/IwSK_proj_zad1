0ecec09c1e4eaece516c3178d9fc8c6b0e4e716c
minemaster1329 <minemaster1329@onet.com.pl>
2022-06-21 18:57:07 +0200

    Dodaj pliki projektów.

---
+++ IwSK_proj_zad1/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace IwSK_proj_zad1
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
